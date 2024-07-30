d10721fd0b16f0ab866f2931a535562a91058884
Beyzanur Ozen <beyzaalways@gmail.com>
2024-07-30 17:22:22 +0300

    Bucket prefab changed according to need

--- Assets/Scripts/AnimalScripts/FarmAnimals/PailController.cs
+++ Assets/Scripts/AnimalScripts/FarmAnimals/PailController.cs
@@ -2,37 +2,39 @@ using UnityEngine;
 
 public class PailController : MonoBehaviour
 {
-    private Transform player;
-    private Transform liquidTransform;
-    private Rigidbody kovaRigidbody;
-    public float swayAmount = 0.5f;
-    public float spillThreshold = 20f;
+    //private Transform player;
+    //private Transform liquidTransform;
+    //private Rigidbody kovaRigidbody;
+    //public float swayAmount = 0.5f;
+    //public float spillThreshold = 20f;
 
-    void Start()
-    {
-        player = transform.GetComponentInParent<Transform>();
-        if (kovaRigidbody == null)
-        {
-            kovaRigidbody = GetComponent<Rigidbody>();
-        }
-    }
+    //void Start()
+    //{
+    //    player = transform.GetComponentInParent<Transform>();
+    //    if (kovaRigidbody == null)
+    //    {
+    //        kovaRigidbody = GetComponent<Rigidbody>();
+    //    }
+    //}
 
-    void Update()
-    {
-        // Player hareketine baðlý olarak kova sallanmasý
-        Vector3 sway = new Vector3(Mathf.Sin(Time.time) * swayAmount, 0, Mathf.Cos(Time.time) * swayAmount);
-        kovaRigidbody.MovePosition(player.position + player.forward + sway);
+    //void Update()
+    //{
+    //    // Player hareketine baðlý olarak kova sallanmasý
+    //    Vector3 sway = new Vector3(Mathf.Sin(Time.time) * swayAmount, 0, Mathf.Cos(Time.time) * swayAmount);
+    //    kovaRigidbody.MovePosition(player.position + player.forward + sway);
 
-        // Sývýnýn dökülmesi kontrolü
-        //if (Vector3.Angle(Vector3.up, transform.up) > spillThreshold)
-        //{
-        //    SpillLiquid();
-        //}
-    }
+    //    // Sývýnýn dökülmesi kontrolü
+    //    //if (Vector3.Angle(Vector3.up, transform.up) > spillThreshold)
+    //    //{
+    //    //    SpillLiquid();
+    //    //}
+    //}
 
-    void SpillLiquid()
-    {
-        // Sývýnýn dökülmesi
-        liquidTransform.localScale = new Vector3(liquidTransform.localScale.x, Mathf.Max(0, liquidTransform.localScale.y - Time.deltaTime), liquidTransform.localScale.z);
-    }
+    //void SpillLiquid()
+    //{
+    //    // Sývýnýn dökülmesi
+    //    liquidTransform.localScale = new Vector3(liquidTransform.localScale.x, Mathf.Max(0, liquidTransform.localScale.y - Time.deltaTime), liquidTransform.localScale.z);
+    //}
+
+
 }
