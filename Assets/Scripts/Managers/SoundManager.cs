1582847efc717bbb494f2887fd398c425822c557
Mehmed Sefa Karataş <mehmedsefakaratasyedek@gmail.com>
2024-08-01 14:35:40 +0300

    Sound manager improved.
    -Sound manager improved with a queue system and more than ne audio sources.

--- Assets/Scripts/Managers/SoundManager.cs
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -1,7 +1,9 @@
 
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class SoundManager : MonoBehaviourSingleton<SoundManager>
 {
@@ -9,23 +11,50 @@ public class SoundManager : MonoBehaviourSingleton<SoundManager>
     [SerializeField] float soundVolume = .5f;
     [SerializeField] List<SoundData> sounds;
 
-    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioSource[] sources;
+    Queue<AudioSource> audioSources;
 
-    public void PlaySound(VoiceType voice, Vector3 pos)
+    private void Start()
     {
+        audioSources = new Queue<AudioSource>();
+        foreach (var source in sources)
+        {
+            audioSources.Enqueue(source);
+        }
+    }
+
+    public void PlaySound(VoiceType voice, Transform parent, Vector3 pos)
+    {
+        var audioSource = audioSources.Dequeue();
+        if (audioSource == null)
+        {
+            print("Not enough audio sources");
+            return;
+        }
+
         audioSource.volume = soundVolume;
         foreach (var sound in sounds)
         {
             if (sound.VoiceType == voice)
             {
+
+                audioSource.transform.parent = parent;
                 audioSource.transform.position = pos;
                 audioSource.clip = sound.Clip;
                 audioSource.Play();
+                StartCoroutine(VoiceFinished(audioSource, sound.Clip.length + .1f));
             }
         }
     }
 
+    IEnumerator VoiceFinished(AudioSource source, float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        audioSources.Enqueue(source);
+    }
 }
+
 
 public enum VoiceType
 {
