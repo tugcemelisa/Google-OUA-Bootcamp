3faa349c97f480bb71487b3a3df7bfd34a4813b8
Mehmed Sefa Karataş <mehmedsefakaratasyedek@gmail.com>
2024-08-02 01:07:59 +0300

    Chicken egg.

---
+++ Assets/Scripts/AnimalScripts/FarmAnimals/Chicken.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Chicken : MonoBehaviour
+{
+    [SerializeField] float minSpawRate = 10;
+
+    [SerializeField] float maxSpawRate = 50;
+
+    [SerializeField] GameObject eggPrefab;
+
+    private void Start()
+    {
+        StartCoroutine(SpawnEgg());
+    }
+
+    IEnumerator SpawnEgg()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(minSpawRate, maxSpawRate));
+            Instantiate(eggPrefab, transform.position, Quaternion.identity);
+        }
+    }
+}
