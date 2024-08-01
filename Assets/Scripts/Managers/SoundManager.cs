b3608b12aac5cec4ef4af01b567ca9f0ed3ee090
Mehmed Sefa Karataş <mehmedsefakaratasyedek@gmail.com>
2024-08-01 12:56:25 +0300

    Sound Manager improved.
    -Sound Manager improved and changed.

--- Assets/Scripts/Managers/SoundManager.cs
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -1,14 +1,53 @@
+
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class SoundManager : MonoBehaviourSingleton<SoundManager>
 {
-    [SerializeField] float soundVolume = 50;
+    [Range(0f, 1f)]
+    [SerializeField] float soundVolume = .5f;
+    [SerializeField] List<SoundData> sounds;
 
+    [SerializeField] AudioSource audioSource;
 
+    public void PlaySound(VoiceType voice, Vector3 pos)
+    {
+        audioSource.volume = soundVolume;
+        foreach (var sound in sounds)
+        {
+            if (sound.VoiceType == voice)
+            {
+                audioSource.transform.position = pos;
+                audioSource.clip = sound.Clip;
+                audioSource.Play();
+            }
+        }
+    }
 
 }
 
 public enum VoiceType
 {
+    YasYasGle,
+    ShepherdYell,
+    Success,
+    FarmAmbiance,
+    CowNormal,
+    CowStrong,
+    Sheep,
+    TorchLightningShort,
+    TorchLightningLong,
+    TorchAttack,
+    TorchAttack2,
+    WolfHowling,
+    WolfScaried,
+}
 
+[Serializable]
+public class SoundData
+{
+    public VoiceType VoiceType;
+    public string Name;
+    public AudioClip Clip;
 }
