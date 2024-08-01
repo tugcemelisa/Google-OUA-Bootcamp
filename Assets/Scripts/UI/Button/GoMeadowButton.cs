eed96b9db5f21a87b3227d050df9559958e70c15
Beyzanur Ozen <beyzaalways@gmail.com>
2024-08-01 15:56:44 +0300

    Button scripts improved
    _Conditions for going to graze and coming back done

--- Assets/Scripts/UI/Button/GoMeadowButton.cs
+++ Assets/Scripts/UI/Button/GoMeadowButton.cs
@@ -1,18 +1,20 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class GoMeadowButton : MonoBehaviour
 {
     Button button;
+    [HideInInspector] public static Action OnGoingMeadowRequest;
 
     private void OnEnable()
     {
         button = GetComponent<Button>();
 
-        button.onClick.AddListener(() => PlayerSimulationController.OnHerdLeaveBarn.Invoke());
+        button.onClick.AddListener(() => OnGoingMeadowRequest.Invoke());
     }
     private void OnDisable()
     {
-        button.onClick.RemoveListener(() => PlayerSimulationController.OnHerdLeaveBarn.Invoke());
+        button.onClick.RemoveListener(() => OnGoingMeadowRequest.Invoke());
     }
 }
--- Assets/Scripts/UI/Button/ReturnVillageButton.cs
+++ Assets/Scripts/UI/Button/ReturnVillageButton.cs
@@ -1,18 +1,20 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ReturnVillageButton : MonoBehaviour
 {
     Button button;
+    [HideInInspector] public static Action OnReturnVillageRequest;
 
     private void OnEnable()
     {
         button = GetComponent<Button>();
 
-        button.onClick.AddListener(() => WolfManager.OnHuntOver.Invoke());
+        button.onClick.AddListener(() => OnReturnVillageRequest.Invoke());
     }
     private void OnDisable()
     {
-        button.onClick.RemoveListener(() => WolfManager.OnHuntOver.Invoke());
+        button.onClick.RemoveListener(() => OnReturnVillageRequest.Invoke());
     }
 }
