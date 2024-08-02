2ea54932feee248eb3607e9cf56e2871a607645c
Mehmed Sefa Karataş <mehmedsefakaratasyedek@gmail.com>
2024-08-02 10:05:15 +0300

    Game Icon size changed. Before wolf inovation.

--- Assets/Scripts/Managers/MeadowWolfManagerInfoHolder.cs
+++ Assets/Scripts/Managers/MeadowWolfManagerInfoHolder.cs
@@ -2,7 +2,10 @@ using UnityEngine;
 
 public class MeadowWolfManagerInfoHolder : MonoBehaviour
 {
+    public Transform circleTransform;
     public Transform[] circleTargets;
     public Transform centerOfTheCircle;
     public Transform[] gettingOutTransforms;
+
+    public Transform[] SpawnPositionsForWolves;
 }
