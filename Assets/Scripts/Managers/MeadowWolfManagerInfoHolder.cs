7b4cea7c3d17dfcd45f53ad793794b1278e4dd96
Mehmed Sefa Karataş <mehmedsefakaratasyedek@gmail.com>
2024-08-02 10:24:11 +0300

    Navmesh baking finished.

--- Assets/Scripts/Managers/MeadowWolfManagerInfoHolder.cs
+++ Assets/Scripts/Managers/MeadowWolfManagerInfoHolder.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class MeadowWolfManagerInfoHolder : MonoBehaviour
 {
-    public Transform circleTransform;
     public Transform[] circleTargets;
     public Transform centerOfTheCircle;
     public Transform[] gettingOutTransforms;
