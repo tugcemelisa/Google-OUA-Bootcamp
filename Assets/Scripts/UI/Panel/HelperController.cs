4a37be11cf591376a617053e1e868a541022bdcb
Mehmed Sefa Karataş <mehmedsefakaratasyedek@gmail.com>
2024-08-02 12:54:58 +0300

    Helper finished.

---
+++ Assets/Scripts/UI/Panel/HelperController.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class HelperController : MonoBehaviourSingletonPersistent<HelperController>
+{
+    [SerializeField] List<PanelData> data;
+
+    [System.Serializable]
+    class PanelData
+    {
+        public string name;
+        public GameObject gameObject;
+        public HelpType helpType;
+    }
+
+    public void ShowHelper(HelpType type)
+    {
+        foreach (var item in data)
+        {
+            item.gameObject.SetActive(false);
+        }
+        foreach (var item in data)
+        {
+            if (item.helpType == type)
+            {
+                item.gameObject.SetActive(true);
+                break;
+            }
+        }
+    }
+}
+
+public enum HelpType
+{
+    FindLiveStock,
+    GoGrazePanel,
+    TakeAnimalGrazePanel,
+    REstPanel,
+    DefendPanel,
+    CollectPanel,
+    ReturnPanel,
+    BarnPanel,
+    FindClient,
+    BuyPanel
+}
